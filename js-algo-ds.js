69a42bd7f5f33651576c8050335b30ae883aaaa5
Faiz <83203236+faiz@users.noreply.github.com>
2024-11-22 23:33:58 +0800

    init

---
+++ js-algo-ds.js
@@ -0,0 +1,24 @@
+
+console.log("Let's build a pyramid using JavaScript basics:");
+console.log("    #    ");
+console.log("   ###   ");
+console.log("  #####  ");
+console.log(" ####### ");
+console.log("#########");
+
+// variable initializatin
+let character;
+/*
+Difference between "" and ''
+both can define strings. 
+The choice is often based on style or to avoid escaping quotes within the string. For example:
+
+-Using single quotes: 'He said "hello".'
+-Using double quotes: "It's a sunny day."
+
+ */
+let welcome = "Hello";
+let goodbye = 'See ya.';
+
+// convention name for long variable use camel case
+// exmaple: let variableOne;
