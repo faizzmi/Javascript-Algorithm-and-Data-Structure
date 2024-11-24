b63bc66bf3d8120fd266ec310f48874e2f8a9372
Faiz <83203236+faiz@users.noreply.github.com>
2024-11-24 23:07:15 +0800

    pyramid

---
+++ pyramid.js
@@ -0,0 +1,13 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+    return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 1; i <= count; i++){
+    rows.push(padRow(i, count));
+}
+
+console.log(rows);
