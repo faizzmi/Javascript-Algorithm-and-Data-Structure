bdf35ebfc242b23dc2ea83ae0a1552f68192aaa3
Faiz <83203236+faiz@users.noreply.github.com>
2024-11-25 22:36:45 +0800

    lit

--- pyramid.js
+++ pyramid.js
@@ -6,8 +6,21 @@ function padRow(rowNumber, rowCount) {
     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-for (let i = 1; i <= count; i++){
-    rows.push(padRow(i, count));
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+if (5 < 10) {
+    console.log("Condition is true");
+} else if (5 < 10) {
+    console.log("5 is less than 10")
 }
 
-console.log(rows);
+let result = ""
+
+for (const row of rows) {
+    result = result + row + "\n";
+}
+
+console.log(result);
