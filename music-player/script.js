3d9c5e75217c01e178e77c3b56bad54ba4156bcf
Faiz <83203236+faiz@users.noreply.github.com>
2024-12-13 16:04:08 +0800

    play next song function

--- music-player/script.js
+++ music-player/script.js
@@ -95,8 +95,29 @@ const playSong = (id) => {
     } else {
         audio.currentTime = userData?.songCurrentTime;
     }
+    userData.currentSong = song;
+    playButton.classList.add("playing");
 
+    audio.play();
+};
 
+const pauseSong = () => {
+    userData.songCurrentTime = audio.currentTime;
+
+    playButton.classList.remove("playing");
+    audio.pause();
+};
+
+const playNextSong = () => {
+
+    if (userData?.currentSong === null) {
+        playSong(userData?.songs[0].id);
+    } else {
+        const currentSongIndex = getCurrentSongIndex();
+        const nextSong = userData?.songs[currentSongIndex + 1];
+
+        playSong(nextSong.id);
+    }
 };
 
 const renderSongs = (array) => {
@@ -104,7 +125,7 @@ const renderSongs = (array) => {
         .map((song) => {
             return `
       <li id="song-${song.id}" class="playlist-song">
-      <button class="playlist-song-info">
+      <button class="playlist-song-info" onclick="playSong(${song.id})">
           <span class="playlist-song-title">${song.title}</span>
           <span class="playlist-song-artist">${song.artist}</span>
           <span class="playlist-song-duration">${song.duration}</span>
@@ -120,6 +141,20 @@ const renderSongs = (array) => {
 
     playlistSongs.innerHTML = songsHTML;
 };
+
+const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
+
+playButton.addEventListener("click", () => {
+    if (userData?.currentSong === null) {
+        playSong(userData?.songs[0].id);
+    } else {
+        playSong(userData?.currentSong.id);
+    }
+});
+
+pauseButton.addEventListener("click", pauseSong);
+
+
 
 const sortSongs = () => {
     userData?.songs.sort((a, b) => {
