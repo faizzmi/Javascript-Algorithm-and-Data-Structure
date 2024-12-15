9172daa3d63afddf86be90cecf919c34277cbf4c
Faiz <83203236+faiz@users.noreply.github.com>
2024-12-15 23:13:41 +0800

    init

---
+++ numeral converter/script.js
@@ -0,0 +1,47 @@
+const input = document.getElementById("number");
+const convertButton = document.getElementById("convert-btn");
+const output = document.getElementById("output");
+const romanNumerals = [
+    { value: 1000, numeral: 'M' },
+    { value: 900, numeral: 'CM' },
+    { value: 500, numeral: 'D' },
+    { value: 400, numeral: 'CD' },
+    { value: 100, numeral: 'C' },
+    { value: 90, numeral: 'XC' },
+    { value: 50, numeral: 'L' },
+    { value: 40, numeral: 'XL' },
+    { value: 10, numeral: 'X' },
+    { value: 9, numeral: 'IX' },
+    { value: 5, numeral: 'V' },
+    { value: 4, numeral: 'IV' },
+    { value: 1, numeral: 'I' }
+];
+
+function validation (){
+    const number = input.value;
+
+    if(number === ""){
+        output.textContent = "Please enter a valid number"
+    } else if(number < 0){
+        output.textContent = "Please enter a number greater than or equal to 1"
+    } else if (number > 4000) {
+        output.textContent = "Please enter a number less than or equal to 3999"
+    } else {
+        output.textContent = numberToRoman(number);
+    }
+
+}
+
+const numberToRoman=(num)=>{
+    let result = '';
+    for(let i = 0; i < romanNumerals.length; i++){
+        while(num >= romanNumerals[i].value){
+            result += romanNumerals[i].numeral;
+            num -= romanNumerals[i].value;
+        }
+    }
+
+    return result;
+}
+
+convertButton.addEventListener("click", validation);
